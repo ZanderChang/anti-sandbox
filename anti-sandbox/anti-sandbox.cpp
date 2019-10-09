53377d567008956903e7927315de89a76d99a680
ZanderZhang <zhangzheng232@hotmail.com>
2019-10-09 16:00:14 +0800

    获取CPU核心数和温度

--- anti-sandbox/anti-sandbox.cpp
+++ anti-sandbox/anti-sandbox.cpp
@@ -3,19 +3,251 @@
 
 #include "pch.h"
 #include <iostream>
+#include <windows.h>
+#include <sysinfoapi.h>
+#include <comdef.h>
+#include <Wbemidl.h>
+#include <ShlObj.h>
+
+using namespace std;
+
+#pragma comment(lib, "wbemuuid.lib")
+
+// 获取当前系统的时钟间隔值（频率倒数）
+DWORD getSystemClockInterval()
+{
+	// Sysinternal的ClockRes
+	// x86上最小0.500 ms，最大15.625 ms
+	DWORD adjustment, clockInterval;
+	BOOL  adjustmentDisabled;
+	GetSystemTimeAdjustment(&adjustment,
+		&clockInterval,
+		&adjustmentDisabled);
+	return clockInterval / 10000;
+}
+
+// 获取CPU核心数
+INT getCPUCores()
+{
+	INT i = 0;
+	_asm { // x64编译模式下不支持__asm的汇编嵌入
+		mov eax, dword ptr fs : [0x18]; // TEB
+		mov eax, dword ptr ds : [eax + 0x30]; // PEB
+		mov eax, dword ptr ds : [eax + 0x64];
+		mov i, eax;
+	}
+	return i;
+}
+
+// 获取CPU温度（需要管理员权限）
+// Get-WMIObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+// https://docs.microsoft.com/en-us/windows/win32/wmisdk/example--getting-wmi-data-from-the-local-computer
+DOUBLE getCPUTemperature()
+{
+	HRESULT hres;
+	DOUBLE res = 0;
+
+	do 
+	{
+		// Step 1: --------------------------------------------------
+	// Initialize COM. ------------------------------------------
+
+		hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+		if (FAILED(hres))
+		{
+			// cout << "Failed to initialize COM library. Error code = 0x" << hex << hres << endl;
+			break;                  // Program has failed.
+		}
+
+		// Step 2: --------------------------------------------------
+		// Set general COM security levels --------------------------
+
+		hres = CoInitializeSecurity(
+			NULL,
+			-1,                          // COM authentication
+			NULL,                        // Authentication services
+			NULL,                        // Reserved
+			RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
+			RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
+			NULL,                        // Authentication info
+			EOAC_NONE,                   // Additional capabilities 
+			NULL                         // Reserved
+		);
+
+		if (FAILED(hres))
+		{
+			// cout << "Failed to initialize security. Error code = 0x" << hex << hres << endl;
+			CoUninitialize();
+			break;                    // Program has failed.
+		}
+
+		// Step 3: ---------------------------------------------------
+		// Obtain the initial locator to WMI -------------------------
+
+		IWbemLocator *pLoc = NULL;
+
+		hres = CoCreateInstance(
+			CLSID_WbemLocator,
+			0,
+			CLSCTX_INPROC_SERVER,
+			IID_IWbemLocator, (LPVOID *)&pLoc);
+
+		if (FAILED(hres))
+		{
+			// cout << "Failed to create IWbemLocator object." << " Err code = 0x" << hex << hres << endl;
+			CoUninitialize();
+			break;                 // Program has failed.
+		}
+
+		// Step 4: -----------------------------------------------------
+		// Connect to WMI through the IWbemLocator::ConnectServer method
+
+		IWbemServices *pSvc = NULL;
+
+		// Connect to the root\cimv2 namespace with
+		// the current user and obtain pointer pSvc
+		// to make IWbemServices calls.
+		hres = pLoc->ConnectServer(
+			// _bstr_t(L"ROOT\\CIMV2"), // Object path of WMI namespace
+			_bstr_t(L"ROOT\\WMI"),
+			NULL,                    // User name. NULL = current user
+			NULL,                    // User password. NULL = current
+			0,                       // Locale. NULL indicates current
+			NULL,                    // Security flags.
+			0,                       // Authority (for example, Kerberos)
+			0,                       // Context object 
+			&pSvc                    // pointer to IWbemServices proxy
+		);
+
+		if (FAILED(hres))
+		{
+			// cout << "Could not connect. Error code = 0x" << hex << hres << endl;
+			pLoc->Release();
+			CoUninitialize();
+			break;                // Program has failed.
+		}
+
+		// cout << "Connected to ROOT\\WMI WMI namespace" << endl;
+
+		// Step 5: --------------------------------------------------
+		// Set security levels on the proxy -------------------------
+
+		hres = CoSetProxyBlanket(
+			pSvc,                        // Indicates the proxy to set
+			RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
+			RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
+			NULL,                        // Server principal name 
+			RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
+			RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+			NULL,                        // client identity
+			EOAC_NONE                    // proxy capabilities 
+		);
+
+		if (FAILED(hres))
+		{
+			// cout << "Could not set proxy blanket. Error code = 0x" << hex << hres << endl;
+			pSvc->Release();
+			pLoc->Release();
+			CoUninitialize();
+			break;               // Program has failed.
+		}
+
+		// Step 6: --------------------------------------------------
+		// Use the IWbemServices pointer to make requests of WMI ----
+
+		// For example, get the name of the operating system
+		IEnumWbemClassObject* pEnumerator = NULL;
+		hres = pSvc->ExecQuery(
+			bstr_t("WQL"),
+			bstr_t("SELECT * FROM MSAcpi_ThermalZoneTemperature"),
+			WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+			NULL,
+			&pEnumerator);
+
+		if (FAILED(hres))
+		{
+			// cout << "Query for operating system name failed." << " Error code = 0x" << hex << hres << endl;
+			pSvc->Release();
+			pLoc->Release();
+			CoUninitialize();
+			break;               // Program has failed.
+		}
+
+		// Step 7: -------------------------------------------------
+		// Get the data from the query in step 6 -------------------
+
+		IWbemClassObject *pclsObj = NULL;
+		ULONG uReturn = 0;
+
+		while (pEnumerator)
+		{
+			HRESULT hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+
+			if (0 == uReturn)
+			{
+				break;
+			}
+
+			VARIANT vtProp;
+
+			// Get the value of the Name property
+			hr = pclsObj->Get(L"CurrentTemperature", 0, &vtProp, 0, 0);
+			res = vtProp.ullVal / 10.0 - 273.15; // 开氏转摄氏
+			VariantClear(&vtProp);
+
+			pclsObj->Release();
+		}
+
+		// Cleanup
+		// ========
+
+		pSvc->Release();
+		pLoc->Release();
+		pEnumerator->Release();
+		CoUninitialize();
+
+	} while (false);
+
+	return res;
+}
+
+BOOL isAdmin()
+{
+	BOOL bElevated = FALSE;
+	HANDLE hToken = NULL;
+
+	// Get current process token
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
+		return FALSE;
+
+	TOKEN_ELEVATION tokenEle;
+	DWORD dwRetLen = 0;
+
+	// Retrieve token elevation information
+	if (GetTokenInformation(hToken, TokenElevation, &tokenEle, sizeof(tokenEle), &dwRetLen))
+	{
+		if (dwRetLen == sizeof(tokenEle))
+		{
+			bElevated = tokenEle.TokenIsElevated;
+		}
+	}
+
+	CloseHandle(hToken);
+	return bElevated;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	if (isAdmin() && IsUserAnAdmin())
+	{
+		printf("[+] Admin\n");
+		printf("[+] %f C\n", getCPUTemperature());
+	}
+	else {
+		printf("[+] Not Admin\n");
+		printf("[+] Cores: %d\n", getCPUCores());
+	}
+
+	return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
